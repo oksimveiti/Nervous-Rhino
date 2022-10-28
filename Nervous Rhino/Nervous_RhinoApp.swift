d9b0ea09d4d126e41f00bfb2edc120d9c1223dba
oksimveiti <semihcettin@gmail.com>
2022-10-28 19:15:42 +0300

    Initial Commit

---
+++ Nervous Rhino/Nervous_RhinoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nervous_RhinoApp.swift
+//  Nervous Rhino
+//
+//  Created by Semih Cetin on 28.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct Nervous_RhinoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
