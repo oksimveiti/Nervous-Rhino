60a8fe832bee988622b4a4a54ec90cb4ddcc6396
oksimveiti <semihcettin@gmail.com>
2022-11-01 01:14:23 +0300

    - create model and view model - create grid view home view - create detail view and list view - add preload recipes - add category pictures - add color scheme

--- Nervous Rhino/Nervous_RhinoApp.swift
+++ Nervous Rhino/Nervous_RhinoApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct Nervous_RhinoApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            RecipeCategoryGridView()
         }
     }
 }
---
+++ Nervous Rhino/Views/RecipesListView.swift
@@ -0,0 +1,45 @@
+//
+//  RecipesListView.swift
+//  Nervous Rhino
+//
+//  Created by Semih Cetin on 28.10.2022.
+//
+
+import SwiftUI
+
+struct RecipesListView: View {
+    @EnvironmentObject private var recipeData: RecipeData
+    let category: MainInformation.Category
+
+    private let listBackgroundColor = AppColor.background
+    private let listTextColor = AppColor.foreground
+    
+    var body: some View {
+        List {
+            ForEach(recipes) { recipe in
+                NavigationLink(recipe.mainInformation.name, destination: RecipeDetailView(recipe: recipe))
+            }
+            .listRowBackground(listBackgroundColor)
+            .foregroundColor(listTextColor)
+        }
+        .navigationTitle(navigationTitle)
+    }
+}
+
+extension RecipesListView {
+    private var recipes: [Recipe] {
+        recipeData.recipes(for: category)
+    }
+    
+    private var navigationTitle: String {
+        "\(category.rawValue) Recipes"
+    }
+}
+
+struct RecipesListView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            RecipesListView(category: .breakfast)
+        }.environmentObject(RecipeData())
+    }
+}
